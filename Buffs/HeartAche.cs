6cce65b1911b06dfa4dc6d77d222d8e614213bb8
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-04-30 11:49:50 -0500

    Update HeartAche.cs

--- Buffs/HeartAche.cs
+++ Buffs/HeartAche.cs
@@ -9,16 +9,5 @@ namespace Nurser.Buffs
         {
             base.SetStaticDefaults();
         }
-
-        public override void Update(Player player, ref int buffIndex)
-        {
-            player.buffTime[buffIndex]--;
-            if (player.buffTime[buffIndex] <= 0) //this doesn't do anything sorta
-            {
-                player.DelBuff(buffIndex);
-                buffIndex--;
-            }
-        }
     }
 }
-// btw this is mostly bullshit so no need for that this is just a place holder buff that all it does is stays in player for sixty seconds, also for some reason in the newest version of tmod i can't do Description.SetDefault because no i guess.
