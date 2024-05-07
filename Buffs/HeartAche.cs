c1262c69f3f42216705beddefd9c3b200fcdbe24
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-07 14:58:55 -0500

    Update HeartAche.cs

--- Buffs/HeartAche.cs
+++ Buffs/HeartAche.cs
@@ -9,5 +9,12 @@ namespace Nurser.Buffs
         {
             base.SetStaticDefaults();
         }
+
+        //buff is used as an placeholder for time
+        //you could make this so it also give the player slower health regen values
+        public override void Update(ref int buffIndex)
+        {
+            Main.LocalPlayer.lifeRegen -= 2; // lower health regen by 2
+        }
     }
 }
